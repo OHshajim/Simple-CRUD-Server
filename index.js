30a6af824580d750206b477316917871e9c5e88c
OHshajim <ajshajimmax@gmail.com>
2024-04-22 19:25:32 +0600

    data get

--- index.js
+++ index.js
@@ -30,9 +30,11 @@ async function run() {
 
         const userCollection = client.db('usersDB').collection('users');
 
-        // app.get('users',async(req,res)=>{
-        //     res.send()
-        // })
+        app.get('/users',async(req,res)=>{
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
